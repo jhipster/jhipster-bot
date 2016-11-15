af8ae0f8d368f62e2c069c4de5ad63efa9bf0915
CarlTheBot <undefined>
2016-11-15 15:22:46 +0100

    fix tests

--- test/api/publisher_api_test.js
+++ test/api/publisher_api_test.js
@@ -1,17 +1,18 @@
+'use strict';
+
+var expect = require('chai').expect;
+var assert = require('assert');
 var api = require('../../lib/api/api');
 var request = require('supertest');
 
-
-var body = {};
-request(api)
-    .post('/publisher/directory')
-    .send()
-    .expect(500)
-    .end(function(err, res){
-        if(err){
-            console.log(err);
-        }else{
-            console.log(res);
-        }
-
+describe('Publisher Router', () => {
+    describe('When trying to publish when not authenticated', () => {
+        it('POST /publisher/directory send an error 500', () => {
+            var body = {};
+            request(api)
+                .post('/publisher/directory')
+                .send()
+                .expect(500)
+        });
     });
+});
